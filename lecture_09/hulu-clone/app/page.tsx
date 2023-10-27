584a22f3b5b10994d05e4b58d60e4b583d480c34
bojanangelis <bojanangeleski98@gmail.com>
2023-10-27 22:09:54 +0200

    update

--- lecture_09/hulu-clone/app/page.tsx
+++ lecture_09/hulu-clone/app/page.tsx
@@ -3,6 +3,12 @@ import Header from '@/components/Header'
 import Main from '@/components/Main'
 import { RecoilRoot } from 'recoil'
 
+//how to start app
+// npx create-next-app@latest my-app
+// tailwindcss i nextjs
+// database https://developer.themoviedb.org/reference/intro/authentication
+// recoil https://recoiljs.org/docs/getting-started/
+
 export default function Home() {
   return (
     <>
